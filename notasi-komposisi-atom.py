a8b0c62f9157fd42cf1e9d51f714c15f7a43bc2a
Evieepk12 <eviepk12@gmail.com>
2022-01-17 23:18:16 +0700

    simpe program for my school

---
+++ notasi-komposisi-atom.py
@@ -0,0 +1,12 @@
+def notasiKomposisiAtom():
+    soal = int(input("Tulis nomor proton di bawah kiri lambang atom :"))
+    protonElektron = soal 
+
+    soalNeutron = int(input("Tulis nomor massa di atas kiri lambang atom :"))
+    neutron = soalNeutron - protonElektron
+
+    print("Proton : ", protonElektron)
+    print("Elektron : ", protonElektron)
+    print("Neutron :", neutron)
+
+notasiKomposisiAtom()
