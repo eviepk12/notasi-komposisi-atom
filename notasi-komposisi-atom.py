cd0b13edd0e14a35e7035be1089adc115a3274d7
Evieepk12 <eviepk12@gmail.com>
2022-01-21 22:41:39 +0700

    added a switch component

--- notasi-komposisi-atom.py
+++ notasi-komposisi-atom.py
@@ -9,4 +9,13 @@ def notasiKomposisiAtom():
     print("Elektron : ", protonElektron)
     print("Neutron :", neutron)
 
-notasiKomposisiAtom()
+print ("1. Notasi Komposisi Atom")
+option = int(input("Ketik nomor yang diinginkan :"))
+
+match option:
+    case 1:
+        notasiKomposisiAtom()
+        exit()
+    case _: 
+        print("Pilih yang bener!!")
+
